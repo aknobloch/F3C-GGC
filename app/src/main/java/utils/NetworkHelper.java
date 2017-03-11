173f7ae3958fecb237eb3548cc473512dc89a3c9
Aaron <aaronknobloch92@gmail.com>
2017-03-11 17:38:17 -0500

    Adds helper files

---
+++ app/src/main/java/utils/NetworkHelper.java
@@ -0,0 +1,32 @@
+package utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by Aaron K on 3/11/2017.
+ */
+
+public class NetworkHelper
+{
+    public static boolean hasNetworkAccess(Context context)
+    {
+        ConnectivityManager cm = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        try
+        {
+            NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+            return activeNetwork != null &&
+                    activeNetwork.isConnectedOrConnecting();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+}
