49a92d50724c35d49b52b2a5d0349371c9463ee4
Aaron <aaronknobloch92@gmail.com>
2017-03-12 14:22:04 -0400

    Adds spoken message to the MessageHelper

--- app/src/main/java/utils/MessageHelper.java
+++ app/src/main/java/utils/MessageHelper.java
@@ -1,9 +1,12 @@
 package utils;
 
 import android.content.Context;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Aaron K on 3/12/2017.
@@ -11,6 +14,10 @@ import android.widget.Toast;
 
 public class MessageHelper
 {
+
+    private static TextToSpeech speaker = null;
+    private static final String SPEAKER_ID = "MessageHelperAlert";
+
     /***
      * Creates a toast with the given message and displays it in the context
      * @param context - The context to display the message
@@ -34,5 +41,38 @@ public class MessageHelper
     {
         Snackbar displayMessage = Snackbar.make(view, message, Snackbar.LENGTH_LONG);
         displayMessage.show();
+    }
+
+    /***
+     * Reads the given message through the phone speaker
+     * @param context - The context this method was called from.
+     * @param message - The message to read aloud.
+     */
+    public static void speakMessage(Context context, String message)
+    {
+        if(speaker == null)
+        {
+            initializeSpeaker(context, message);
+        }
+        else
+        {
+            speaker.speak(message, TextToSpeech.QUEUE_FLUSH, null, SPEAKER_ID);
+        }
+    }
+
+    /***
+     * Initializes the speaker, and reads the given message once it is ready.
+     * @param context - The context this method was called from.
+     * @param message - The message to read aloud.
+     */
+    private static void initializeSpeaker(Context context, final String message)
+    {
+        speaker = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status)
+            {
+                speaker.speak(message, TextToSpeech.QUEUE_FLUSH, null, SPEAKER_ID);
+            }
+        });
     }
 }
