ee4b7e2732ccd18f170dd62a064f75abc50b6f37
Aaron <aaronknobloch92@gmail.com>
2017-03-12 13:54:33 -0400

    Broadens ToastHelper to handle Snackbars

--- app/src/main/java/utils/MessageHelper.java
+++ app/src/main/java/utils/MessageHelper.java
@@ -1,13 +1,15 @@
 package utils;
 
 import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.widget.Toast;
 
 /**
  * Created by Aaron K on 3/12/2017.
  */
 
-public class ToastHelper
+public class MessageHelper
 {
     /***
      * Creates a toast with the given message and displays it in the context
@@ -21,5 +23,16 @@ public class ToastHelper
                 message,
                 Toast.LENGTH_SHORT);
         errorToast.show();
+    }
+
+    /***
+     * Creates a snackbar with the given message and displays it in the view
+     * @param view - The view to display the message
+     * @param message - The message to toast.
+     */
+    public static void snackbarAlert(View view, String message)
+    {
+        Snackbar displayMessage = Snackbar.make(view, message, Snackbar.LENGTH_LONG);
+        displayMessage.show();
     }
 }
